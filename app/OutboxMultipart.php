0f8137f1e13fd8de4fe2a785f6d16b0e86e55191
Miftah Afina <miftah.afina@gmail.com>
2015-06-26 09:17:11 +0700

    Penambahan model OutboxMultipart

---
+++ app/OutboxMultipart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OutboxMultipart extends Model
+{
+    protected $table = 'outbox_multipart';
+
+    protected $primaryKey = 'ID';
+
+    public $timestamp = false;
+}
