f39678cfeccc247ff36d9ce50f4b312e377726bd
Miftah Afina <miftah.afina@gmail.com>
2015-06-26 08:40:55 +0700

    Penambahan migration create contact table

---
+++ database/migrations/2015_06_26_083832_create_contact_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->string('nama');
+            $table->string('ponsel', 20);
+            $table->text('keterangan');
+            $table->integer('user_id')
+                ->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contact');
+    }
+}
