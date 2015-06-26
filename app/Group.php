9b30a72f3edd4f1cf9f8a72bcec349240fdd0b97
Miftah Afina <miftah.afina@gmail.com>
2015-06-26 09:10:37 +0700

    Penambahan model Group

---
+++ app/Group.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    protected $table = 'group';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nama',
+        'keterangan',
+        'user_id'
+    ];
+
+    public function group()
+    {
+        return $this->belongsToMany('App\Contact', 'contact_group', 'group_id', 'contact_id');
+    }
+}
