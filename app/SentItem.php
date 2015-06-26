b572ed0e52dbbaa553ca515731bd04f8206b5b46
Miftah Afina <miftah.afina@gmail.com>
2015-06-26 09:18:15 +0700

    Penambahan model SentItem

---
+++ app/SentItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SentItem extends Model
+{
+    protected $table = 'sentitems';
+
+    protected $primaryKey = 'ID';
+
+    public $timestamp = false;
+}
