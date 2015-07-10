cdff5b334dc2b23fece82c2ccc8ac80f7161f288
Miftah Afina <miftah.afina@gmail.com>
2015-07-10 08:52:20 +0700

    Pengubahan form filter menjadi form generator laravelcollective pada inbox.index, outbox.index dan sentitem.index

--- resources/views/inbox/index.blade.php
+++ resources/views/inbox/index.blade.php
@@ -55,18 +55,18 @@
     </div>
 
     <div class="col-md-6">
-        <form class="form-inline pull-right" action="{{ route('inbox.index') }}" method="get">
+        {!! Form::open(['method' => 'get', 'class' => 'form-inline pull-right']) !!}
+                {!! Form::hidden('sort', $sort) !!}
+                {!! Form::hidden('mode', $mode) !!}
             <div class="form-group">
-                <input type="text" class="form-control input-sm" id="search" name="cari" placeholder="Pencarian..." value="{{ $cari }}">
-                <input type="text" class="form-control input-sm datepicker-month" id="cari_bulan" name="cari_bulan" placeholder="Bulan" value="{{ $cari_bulan }}">
-                <input type="hidden" name="sort" value="{{ $sort }}">
-                <input type="hidden" name="mode" value="{{ $mode }}">
+                {!! Form::text('cari', $cari, ['id' => 'search', 'class' => 'form-control input-sm', 'placeholder' => 'Pencarian...']) !!}
+                {!! Form::text('cari_bulan', $cari_bulan, ['id' => 'cari_bulan', 'class' => 'form-control input-sm datepicker-month', 'placeholder' => 'Bulan']) !!}
             </div>
             <button type="submit" class="btn btn-default btn-sm">
                 <span class="glyphicon glyphicon-search"></span>
                 Cari
             </button>
-        </form>
+        {!! Form::close() !!}
     </div>
 </div><br>
 
