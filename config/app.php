fa2919b7a2111239fe1d90d2a200f2532942f2e9
Miftah Afina <miftah.afina@gmail.com>
2015-07-14 21:46:24 +0700

    Instalasi dan konfigurasi debugbar

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
          * Additional Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class
 
     ],
 
@@ -202,6 +203,7 @@ return [
          */
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class
 
     ],
 
