22f5fdf8e315f4963e2a9845b35ef7c5829511ed
Miftah Afina <miftah.afina@gmail.com>
2015-07-26 11:01:26 +0700

    Pengubahan tahun credit pada footer

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <hr>
         <p class="text-muted text-right">
-            &copy; {{ date('Y') }} <a href="http://www.miftahafina.com" target="_blank">Miftah Afina</a>. Hak cipta terpelihara <br>
+            &copy; {{ '2014 - '.date('Y') }} <a href="http://www.miftahafina.com" target="_blank">Miftah Afina</a>. Hak cipta terpelihara <br>
             <small>Waktu pemuatan halaman {{ round((microtime(true) - LARAVEL_START),4) }} detik.</small>
         </p>
     </div>
