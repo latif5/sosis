16d939d290dfe9a5ceec8b94cb70c9198083a3e9
Miftah Afina <miftah.afina@gmail.com>
2015-08-13 09:38:04 +0700

    Penambahan migration tabel psb

---
+++ database/migrations/2015_08_13_092845_create_psb_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePsbTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('psb', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->datetime('tanggal');
+            $table->string('ponsel');
+            $table->string('santri');
+            $table->string('no_pendaftaran');
+            $table->string('jumlah');
+            $table->string('tanggal_kirim');
+            $table->string('pengirim');
+            $table->string('keperluan');
+            $table->enum('status', ['Belum', 'Sudah', 'Tunda'])
+                ->default('Belum');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('psb');
+    }
+}
