ec241e2a8fd5d2817b7ab4fafba7ce0fa8e3749f
Miftah Afina <miftah.afina@gmail.com>
2015-08-13 15:39:12 +0700

    Mengubah pesan balasan jika format salah

--- app/Http/Controllers/ConfirmationController.php
+++ app/Http/Controllers/ConfirmationController.php
@@ -41,7 +41,7 @@ class ConfirmationController extends Controller
             ->where('santri', 'like', "%$cari%")
             ->where('tanggal', 'like', "$cari_bulan%")
             ->orderBy($sort, $mode)
-            ->paginate(10);
+            ->paginate(50);
 
         return view('confirmation.index', compact('confirmation_all', 'sort', 'mode', 'status', 'cari', 'cari_bulan'));
     }
